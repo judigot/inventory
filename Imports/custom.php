24a4912a282712dea1d098a157839ed1ecbad6eb
judigot <judigot@gmail.com>
2019-07-17 12:27:28 +0800

    Delete

--- Imports/custom.php
+++
@@ -1,4 +0,0 @@
-<!--General CSS-->
-<link href="Assets/css/custom.css" rel="stylesheet" type="text/css"/>
-<!--General JavaScript-->
-<script src="Assets/js/custom.js"></script>
--- README.md
+++
@@ -1,5 +0,0 @@
-# Inventory and POS System with Sales Report
-
-Demo: http://inventoryapp.epizy.com/login
-
-Password for both users is "123"
