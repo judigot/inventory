a589ed71ac5c2d59a961b4e01f059dbbdb358fd7
Igot <jigot@avaya.com>
2020-06-04 18:01:35 +0800

    Commit

--- _receipt.php
+++ _receipt.php
@@ -14,7 +14,7 @@ if (isset($_POST['print']) || $static) {
         $orderIds = json_decode($_POST["data"]["orderIds"]);
     }
 
-    ?>
+?>
     <!DOCTYPE html>
     <html lang="en">
 
@@ -39,7 +39,7 @@ if (isset($_POST['print']) || $static) {
                 margin: 0%;
                 width: 100%;
                 text-align: center;
-                font-size: 10px;
+                font-size: 13px;
                 font-family: Lucida Console;
                 /* font-family: "FakeReceipt-Regular"; */
                 -webkit-print-color-adjust: exact;
@@ -59,8 +59,13 @@ if (isset($_POST['print']) || $static) {
             .grid-container {
                 /* padding: 0px 10px 0px 10px; */
                 padding: 2%;
-                margin: auto;
-                width: max-content;
+                display: grid;
+                grid-gap: 10px;
+                grid-template-columns: repeat(2, 1fr);
+            }
+
+            .lefty {
+                float: left;
             }
 
             .righty {
@@ -83,120 +88,111 @@ if (isset($_POST['print']) || $static) {
             }
 
             td {
-                /* padding: 10px 0px 10px 0px; */
+                padding: 10px 0px 10px 0px;
             }
 
             .ordered-products>thead>tr>th,
             .ordered-products>tbody>tr>td {
                 border: 1px dashed black;
             }
-
-            .total-row {
-                border-top: 2px double black;
-            }
         </style>
     </head>
 
     <body>
         <div class="grid-container">
-            <div class="grid-column">
-                <div class="header">
-                    <div>
-                        <h1><?php echo $appName; ?></h1>
-                    </div>
-                    <br>
-                    <br>
-                    <div>
-                        <span class="righty">Printed on: <?php echo date("F j, Y"); ?></span>
-                    </div>
-                </div>
-                <br><br>
-                <div class="content">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Order ID</th>
-                                <th>Customer</th>
-                                <th>Order</th>
-                                <th>Order Date</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
 
-                            $result = Database::read($connection, "SELECT `$app_order`.`order_id`, `$app_order`.`customer_id`, CONCAT(`$app_customer`.`first_name`, ' ', `$app_customer`.`last_name`) AS `customer_name`, `$app_order`.`order_date` FROM `$app_order` INNER JOIN `$app_customer` ON `$app_order`.`customer_id`=`$app_customer`.`customer_id` WHERE `order_id` IN ('" . implode("', '", $orderIds) . "') ORDER BY `order_id` ASC;");
+            <?php
+            for ($i = 0; $i < 2; $i++) {
+            ?>
+                <div class="grid-column">
+                    <div class="header">
+                        <div>
+                            <span>PD FARM</span>
+                            <br>
+                            <span>ORDER SLIP</span>
+                        </div>
+                        <br>
+                        <div class="lefty"><?php echo $i === 0 ? "Original" : "Duplicate" ?></div>
+                        <br>
+                        <br>
+                        <div>
+                            <span class="righty">Printed on: <?php echo date("F j, Y"); ?></span>
+                        </div>
+                    </div>
+                    <br><br>
+                    <div class="content">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Customer</th>
+                                    <th>Order</th>
+                                    <th>Order Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
 
-                            foreach ($result as $value) {
+                                $result = Database::read($connection, "SELECT `$app_order`.`order_id`, `$app_order`.`customer_id`, CONCAT(`$app_customer`.`first_name`, ' ', `$app_customer`.`last_name`) AS `customer_name`, `$app_order`.`order_date` FROM `$app_order` INNER JOIN `$app_customer` ON `$app_order`.`customer_id`=`$app_customer`.`customer_id` WHERE `order_id` IN ('" . implode("', '", $orderIds) . "') ORDER BY `order_id` ASC;");
+
+                                foreach ($result as $value) {
 
                                 ?>
-                                <tr>
-                                    <td><?php echo $value["order_id"]; ?></td>
-                                    <td><?php echo $value["customer_name"]; ?></td>
-                                    <td>
+                                    <tr>
+                                        <td><?php echo $value["customer_name"]; ?></td>
+                                        <td>
 
-                                        <table class="ordered-products">
-                                            <thead>
-                                                <tr>
-                                                    <th>Quantity</th>
-                                                    <th>Product</th>
-                                                    <th>Price</th>
-                                                    <th>Amount</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <?php
-
-                                                $products = Database::read($connection, "SELECT `$app_order_product`.*, `$app_product`.`product_name` FROM `$app_order_product` INNER JOIN `$app_product` ON `$app_order_product`.`product_id`=`$app_product`.`product_id` WHERE `order_id` = '{$value["order_id"]}'");
-
-                                                $totalQuantity = 0;
-                                                $totalAmount = 0;
-
-                                                foreach ($products as $item) {
-
-                                                    $totalQuantity += $item["quantity"];
-                                                    $totalAmount += ($item["product_price"] * $item["quantity"]) - ($item["discount"] ? $item["discount"] : 0);
-
-                                                    ?>
+                                            <table class="ordered-products">
+                                                <thead>
                                                     <tr>
-                                                        <td><?php echo $item["quantity"] ?></td>
-                                                        <td><?php echo $item["product_name"] ?></td>
-                                                        <td><?php echo "₱" . Tools::monetize(true, $item["product_price"]) ?></td>
-                                                        <td>
-                                                            <?php echo "₱" . Tools::monetize(true, $item["product_price"] * $item["quantity"]); ?>
-                                                            <?php echo $item["discount"] > 0 ? " (" . "₱" . Tools::monetize(true, $item["discount"]) . " discount)" : ""; ?>
-                                                        </td>
+                                                        <th>Quantity</th>
+                                                        <th>Product</th>
+                                                        <th>Price</th>
+                                                        <th>Amount</th>
                                                     </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <?php
 
-                                                <?php } ?>
+                                                    $products = Database::read($connection, "SELECT `$app_order_product`.*, `$app_product`.`product_name` FROM `$app_order_product` INNER JOIN `$app_product` ON `$app_order_product`.`product_id`=`$app_product`.`product_id` WHERE `order_id` = '{$value["order_id"]}'");
 
-                                                <?php
+                                                    $totalQuantity = 0;
+                                                    $totalAmount = 0;
 
-                                                if (count($products) > 1) {
+                                                    foreach ($products as $item) {
+                                                        $totalQuantity += $item["quantity"];
+                                                        $totalAmount += $item["product_price"] * $item["quantity"];
+
                                                     ?>
+                                                        <tr>
+                                                            <td><?php echo $item["quantity"] ?></td>
+                                                            <td><?php echo $item["product_name"] ?></td>
+                                                            <td><?php echo "₱" . Tools::monetize(true, $item["product_price"]) ?></td>
+                                                            <td><?php echo "₱" . Tools::monetize(true, $item["product_price"] * $item["quantity"]) ?></td>
+                                                        </tr>
 
-                                                    <tr class="total-row">
+                                                    <?php } ?>
+                                                    <tr>
                                                         <td><?php echo $totalQuantity; ?></td>
                                                         <td>-</td>
                                                         <td>-</td>
                                                         <td><?php echo "₱" . Tools::monetize(true, $totalAmount); ?></td>
                                                     </tr>
 
-                                                <?php } ?>
+                                                </tbody>
+                                            </table>
 
-                                            </tbody>
-                                        </table>
+                                        </td>
+                                        <td><?php echo date('F j, Y', strtotime($value["order_date"])); ?></td>
+                                    </tr>
 
-                                    </td>
-                                    <td><?php echo date('F j, Y', strtotime($value["order_date"])); ?></td>
-                                </tr>
-
-                            <?php } ?>
-                        </tbody>
-                    </table>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <br>
+                    <div id="acknowledgement"><span>Checked & Received By: __________</span></div>
                 </div>
-                <br>
-                <div id="acknowledgement"><span>Checked & Received By: __________</span></div>
-            </div>
+            <?php } ?>
         </div>
     </body>
 
