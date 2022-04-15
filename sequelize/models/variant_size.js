14feb8f9714582beecc1ed1828a1caefd569e7f5
judigot <judigot@github.com>
2022-04-15 14:03:02 +0800

    Commit

--- sequelize/models/variant_size.js
+++ sequelize/models/variant_size.js
@@ -1,46 +1,50 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('variant_size', {
-    id: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
+const Sequelize = require("sequelize");
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define(
+    "variant_size",
+    {
+      id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      size_name: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      category_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        //==========FOREIGN KEY==========//
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE", // Deletes the child rows when parent row is deleted
+        references: {
+          model: "app_product_category",
+          key: "category_id",
+        },
+        //==========FOREIGN KEY==========//
+      },
     },
-    size_name: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'app_product_category',
-        key: 'category_id'
-      }
+    {
+      sequelize,
+      tableName: "variant_sizes",
+      timestamps: false,
+      charset: "utf8",
+      collate: "utf8_general_ci",
+      indexes: [
+        {
+          name: "PRIMARY",
+          unique: true,
+          using: "BTREE",
+          fields: [{ name: "id" }],
+        },
+        {
+          name: "category_id",
+          using: "BTREE",
+          fields: [{ name: "category_id" }],
+        },
+      ],
     }
-  }, {
-    sequelize,
-    tableName: 'variant_sizes',
-    timestamps: false,
-    charset: 'utf8',
-    collate: 'utf8_general_ci',
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "id" },
-        ]
-      },
-      {
-        name: "category_id",
-        using: "BTREE",
-        fields: [
-          { name: "category_id" },
-        ]
-      },
-    ]
-  });
+  );
 };
